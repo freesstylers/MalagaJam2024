88d8ab01a586281a29f569a0ca15444221087d9f
Jose Martin Serrano <josemsdev@gmail.com>
2024-07-28 00:57:11 +0200

    Efecto ebrio

--- Assets/Scripts/InGameUIManager.gd
+++ Assets/Scripts/InGameUIManager.gd
@@ -2,7 +2,7 @@ class_name InGameUIManager
 extends Node
 
 @export var lerp_factor : float = 3
-@onready var speed_bar : ProgressBar = $CanvasLayer/TextureRect/ProgressBar
+@onready var speed_bar : ProgressBeer = $CanvasLayer/TextureRect/ProgressBar
 @onready var score_meter : Label = $CanvasLayer/ScoreBackground/Score
 
 var visual_bar_value : float = 0
@@ -11,7 +11,7 @@ var current_bar_value : float = 0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	visual_bar_value = visual_bar_value + ((current_bar_value-visual_bar_value) * lerp_factor * delta)
-	speed_bar.value = visual_bar_value
+	speed_bar.set_progress(visual_bar_value)
 
 func update_player_velocity(player_velocity):
 	var bar_value = (player_velocity - Globals.MIN_RUNNING_VEL)/(Globals.MAX_RUNNING_VEL - Globals.MIN_RUNNING_VEL) 
---
+++ Assets/Scripts/ProgressBarShaderLogic.gd
@@ -0,0 +1,10 @@
+class_name ProgressBeer
+extends ProgressBar
+
+@export var mat:ShaderMaterial
+var progress
+
+	
+func set_progress(p:float):
+	mat.set_shader_parameter("p", p/100.0)
+	pass
