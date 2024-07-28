03bb8faac8b59cc44ac26c92e5c9a779ee76673f
IgnOry <ignacioory@gmail.com>
2024-07-28 13:19:09 +0200

    Test

--- Assets/Scripts/endgame_ui.gd
+++ Assets/Scripts/endgame_ui.gd
@@ -6,21 +6,32 @@ func _ready():
 	update_text()
 	Globals.player_lost.connect(update_text)
 
-	#$Panel/Panel/VBoxContainer/Premio.text = "[center][font_size={48}]Título[/font_size]
-#[font_size={24}]XXXX[/font_size][/center]"
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func _on_repeat_pressed():
 	Globals.SceneMngr.load_scene(Globals.Scene.MENU_SCENE)
 
 func _on_share_pressed():
-	var url = "https://twitter.com/intent/tweet?text=" + ("¡Mi nivel de cuñado ha sido: %s" % Globals.finalScore + "!\n\nJuega a \"Jefe, la cuenta\" en https://freestylers-studio.itch.io/jefe-la-cuenta").uri_encode()
+	var url = "https://twitter.com/intent/tweet?text=" + ("¡Mi nivel de cuñado ha sido: %s" % Globals.finalScore + "!\n\nSoy un " + prizeTitle + "\n\nJuega a \"Jefe, La Cuenta\" en https://freestylers-studio.itch.io/jefe-la-cuenta").uri_encode()
 	OS.shell_open(url)
 
+var prizeTitle : String = ""
+	
 func update_text():
 	$Panel/Panel/VBoxContainer/Puntuacion.text = "[center][font_size={48}]Puntuación[/font_size]
 	[font_size={36}]%s" % Globals.finalScore + "[/font_size]
 	-----[/center]"
+	
+	if Globals.finalScore < 100:
+		prizeTitle = "Borrachín Novato"
+	elif Globals.finalScore < 200:
+		prizeTitle = "Verbenero Estándar"
+	elif Globals.finalScore < 300:
+		prizeTitle = "Bebedor Habitual"
+	elif Globals.finalScore < 400:
+		prizeTitle = "Cierrabares"
+	elif Globals.finalScore < 500:
+		prizeTitle = "Maestro Cervecero"
+	else:
+		prizeTitle = "Baco Dios del Alcohol"
+	
+	$Panel/Panel/VBoxContainer/Premio.text = "[center][font_size={48}]Título[/font_size]
+[font_size={24}]" + prizeTitle + "[/font_size][/center]"
