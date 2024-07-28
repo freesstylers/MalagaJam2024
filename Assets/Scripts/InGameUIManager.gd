22f63c5ed8cbdf82b22be73d255a5759a481ccae
Jose Martin Serrano <josemsdev@gmail.com>
2024-07-28 02:04:35 +0200

    Texto perder

--- Assets/Scripts/InGameUIManager.gd
+++ Assets/Scripts/InGameUIManager.gd
@@ -4,9 +4,15 @@ extends Node
 @export var lerp_factor : float = 3
 @onready var speed_bar : ProgressBeer = $CanvasLayer/TextureRect/ProgressBar
 @onready var score_meter : Label = $CanvasLayer/ScoreBackground/Score
+@onready var defeat_text : Label = $CanvasLayer/TextoDerrota
 
 var visual_bar_value : float = 0
 var current_bar_value : float = 0
+
+func _ready():
+	defeat_text.scale = Vector2.ZERO
+	Globals.player_lost.connect(on_player_lost)
+	Globals.get_ready_to_run.connect(on_get_ready_to_run)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -19,3 +25,12 @@ func update_player_velocity(player_velocity):
 	
 func update_score(new_score : int):
 	score_meter.text = str(new_score)
+	
+func on_player_lost():
+	var twin = create_tween()
+	twin.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+	twin.tween_property(defeat_text, "scale", Vector2(1,1), 1)
+	
+
+func on_get_ready_to_run(drunk_meter):
+	defeat_text.scale = Vector2.ZERO
